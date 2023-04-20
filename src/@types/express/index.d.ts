2787ddc7e1ce5f229a4595b0c7ee81e93e6cc7c6
GiancarloPessatti <106692510+GiancarloPessatti@users.noreply.github.com>
2023-04-20 15:34:47 -0300

    feat: algo

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,8 +1,3 @@
-import { ICategoryRequest } from "../../Interfaces/contacts";
-import { IPropertyRequest } from "../../Interfaces/properties";
-import { IScheduleRequest } from "../../Interfaces/schedules";
-import { IUserRequest } from "../../Interfaces/users";
-
 declare global {
   namespace Express {
     interface Request {
